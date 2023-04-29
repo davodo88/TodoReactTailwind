388aff9ab51f9d542e0f4252e3ea9fb3f7ca4898
davodo88 <92156631+davodo88@users.noreply.github.com>
2023-04-29 15:59:59 +0200

    computed items

--- src/components/todoComputed.jsx
+++ src/components/todoComputed.jsx
@@ -1,7 +1,7 @@
 const TodoComputed = ({ computedItemsLeft, clearComplete }) => {
   return (
     <section className="flex justify-around p-4 bg-white rounded-md">
-      <span className='text-gray-400'>{computedItemsLeft} items left</span>
+      <span className='text-gray-400'>{`${computedItemsLeft} todos left`}</span>
       <button onClick={clearComplete}
         className='text-gray-400'>Clear complete</button>
     </section>
